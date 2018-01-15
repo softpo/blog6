f147a2edd729188aeb34e58221b6d642cc12f699
Seamile <lanhuermao@gmail.com>
2018-01-15 17:11:18 +0800

    add some views

--- blog6/settings.py
+++ blog6/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#g&89w0#5&n)e=oouqpx(6ku(+&g(#c=&#mb8noclvvnvl_x$y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'blog6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['post/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
